bf09ff2aea840dc2f86cb7cbad4c09b62d6178ce
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-13 08:57:05 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InputANumberFromUserCheckIfTheNumberIsOddOrEven
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number, reminder;
+            Console.WriteLine("Please enter a number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            reminder = number % 2;
+
+            if (reminder == 0)
+                Console.WriteLine("{0} is an Even Number", number);
+            else
+                Console.WriteLine("{0} is an Odd Number", number);
+            Console.ReadLine();
+        }
+    }
+}
